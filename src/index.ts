f3695fce9e0404c00e864ebf0328cc3cb8736300
Alex Jercan <jercan_alex27@yahoo.com>
2025-04-23 12:34:37 +0300

    feat: add ds.h and demo for array

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,82 @@
+const fps = 60;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+
+const root = document.createElement("div");
+root.id = "root";
+document.body.appendChild(root);
+
+const canvas = document.createElement("canvas");
+canvas.width = 800;
+canvas.height = 600;
+root.appendChild(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const MEMORY_SIZE: number = 8192;
+
 let w: WebAssembly.WebAssemblyInstantiatedSource = null;
 
-type SumFunction = (a: number, b: number) => number;
+type InitFunction = (memory: number, size: number) => void;
+type TickFunction = (deltaTime: number) => void;
 
 WebAssembly.instantiateStreaming(fetch("main.wasm"), {
     env: {
-        memory: new WebAssembly.Memory({initial: 256}),
+        memory: new WebAssembly.Memory({ initial: MEMORY_SIZE }),
+        js_width: (): number => { return CANVAS_WIDTH; },
+        js_height: (): number => { return CANVAS_HEIGHT; },
+        js_random: (a: number, b: number): number => { return Math.random() * (b - a) + a; },
+        js_clear_canvas: (): void => {
+            ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        },
+        js_draw_rect: (x: number, y: number, w: number, h: number, color: number): void => {
+            ctx.fillStyle = `#${color.toString(16).padStart(6, '0')}`;
+            ctx.fillRect(x, y, w, h);
+        },
+        js_fprintf: (stream: number, format: number, ...args: number[]): void => {
+            const memory = new Uint8Array((w.instance.exports.memory as WebAssembly.Memory).buffer);
+            let str = "";
+            let i = format;
+            while (memory[i] !== 0) {
+                str += String.fromCharCode(memory[i]);
+                i++;
+            }
+
+            const values = args.map(arg => {
+                return memory[arg];
+            });
+
+            console.log(str, ...values);
+        }
     },
 }).then((value) => {
+    let fpsInterval = 1000 / fps;
+
     w = value;
 
-    let sum = w.instance.exports.sum as SumFunction;
+    let init = w.instance.exports.init as InitFunction;
+    let tick = w.instance.exports.tick as TickFunction;
 
-    console.log(sum(1, 2));
+    const memory = (w.instance.exports.__heap_base as WebAssembly.Global).value;
+
+    init(memory, MEMORY_SIZE);
+
+    let then = Date.now();
+
+    function gameLoop() {
+        requestAnimationFrame(gameLoop);
+
+        let now = Date.now();
+        let elapsed = now - then;
+
+        if (elapsed > fpsInterval) {
+            then = now - (elapsed % fpsInterval);
+
+            tick(elapsed);
+        }
+    }
+
+    requestAnimationFrame(gameLoop);
+}).catch(e => {
+    console.error("Error instantiating the WebAssembly module", e);
 });
--- src/main.c
+++ src/main.c
@@ -1,5 +1,80 @@
-int sum(int a, int b);
+extern int js_fprintf(void *stream, const char *format, ...);
 
-int sum(int a, int b) {
-    return a + b;
+#define DS_NO_STDLIB
+#define DS_DA_INIT_CAPACITY 16
+#define DS_LIST_ALLOCATOR_IMPLEMENTATION
+#define DS_DA_IMPLEMENTATION
+#define DS_NO_TERMINAL_COLORS
+#define fprintf js_fprintf
+#include "ds.h"
+
+extern int js_width();
+extern int js_height();
+extern int js_random(int a, int b);
+
+extern void js_clear_canvas();
+extern void js_draw_rect(int x, int y, int w, int h, int color);
+
+typedef struct rect {
+    int x, y, w, h;
+} rect_t;
+
+void rect_random(int width, int height, rect_t *rect) {
+    int w = js_random(width / 8, width / 4);
+    int h = js_random(height / 8, height / 4);
+    int x = js_random(0, width - w);
+    int y = js_random(0, height - h);
+
+    rect->w = w;
+    rect->h = h;
+    rect->x = x;
+    rect->y = y;
+}
+
+typedef struct piece {
+    rect_t rect;
+    int color;
+    float vx, vy;
+} piece_t;
+
+void piece_random(int width, int height, piece_t *piece) {
+    rect_random(width, height, &piece->rect);
+    piece->color = js_random(0, 0xFFFFFF);
+    piece->vx = js_random(-100, 100) / 100.0f;
+    piece->vy = js_random(-100, 100) / 100.0f;
+}
+
+DS_ALLOCATOR allocator = {0};
+ds_dynamic_array array = {0};
+
+void init(void *memory, unsigned long size) {
+    DS_INIT_ALLOCATOR(&allocator, memory, size);
+    ds_dynamic_array_init_allocator(&array, sizeof(piece_t), &allocator);
+
+    for (unsigned int i = 0; i < 10; i++) {
+        piece_t piece = {0};
+        piece_random(js_width(), js_height(), &piece);
+        ds_dynamic_array_append(&array, &piece);
+    }
+}
+
+void tick(float deltaTime) {
+    js_clear_canvas();
+    for (unsigned int i = 0; i < array.count; i++) {
+        piece_t *piece = NULL;
+        ds_dynamic_array_get_ref(&array, i, (void **)&piece);
+        js_draw_rect(piece->rect.x, piece->rect.y, piece->rect.w, piece->rect.h,
+                     piece->color);
+
+        if (piece->rect.x < 0 || piece->rect.x + piece->rect.w > js_width()) {
+            piece->vx = -piece->vx;
+        }
+
+        if (piece->rect.y < 0 || piece->rect.y + piece->rect.h > js_height()) {
+            piece->vy = -piece->vy;
+        }
+
+        piece->rect.x += piece->vx * deltaTime;
+        piece->rect.y += piece->vy * deltaTime;
+    }
 }
